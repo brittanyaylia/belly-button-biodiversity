9d43e1e003be119287199cdde011b502672d6da6
brittanyaylia <whitakerbrittany18@gmail.com>
2021-05-22 12:02:53 -0700

    code

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,32 @@
-// belly button diversity 
+// step 1: plotly 
+
+var idSelect = d3.select("#selDataset");
+var demographicsTable = d3.select("#sample-metadata");
+var barChart = d3.select("#bar");
+var bubbleChart = d3.select("bubble");
+
+// horizontal bar chart 
+
+//dropdown menu
+function init() {
+
+    // reset data
+    resetData();
+
+    // read in data from json
+    d3.json("data/samples.json").then((data => {
+
+        //populate dropdown 
+        data.names.forEach((name => {
+            var option = idSelect.append("option");
+            option.text(name);
+        }));
+
+        // select firstid from data list
+        var initId = idSelect.property("value")
+
+        // plot
+        plotCharts(initId);
+    }));
+}
 
